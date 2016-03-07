f4bdd385159152c7bca9f12e55d6f359c94cd95f
BMJHayward <brendanhayward85@gmail.com>
2016-03-07 21:52:55 +1100

    add ignore file, basic looping piezo output for arduino

---
+++ arduino/piezosound.ino
@@ -0,0 +1,22 @@
+int piezo = 8;
+int duration = 250;
+int notes[] = {261, 293, 329, 349, 392, 440, 493, 523, 587, 659, 698, 783, 880};
+
+void setup()
+{
+    pinMode(piezo, OUTPUT);
+}
+
+void loop()
+{
+    for (int i = 0; i < 13; i++)
+    {
+        tone(piezo, notes[i], duration);
+        delay(duration);
+    }
+    for (int i = 11; i > 0; --i)
+    {
+        tone(piezo, notes[i], duration);
+        delay(duration);
+    }
+}
