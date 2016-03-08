065025de84198ba7db1a796b4f9e314e2c3fc7c0
BMJHayward <brendanhayward85@gmail.com>
2016-03-08 21:43:28 +1100

    make noisegen.ino compile with arduino IDE

--- arduino/noisegen.ino
+++ arduino/noisegen.ino
@@ -21,34 +21,36 @@ void setup()
 //see: http://arduino.stackexchange.com/questions/6715/audio-frequency-white-noise-generation-using-arduino-mini-pro
 void generateNoise_a()
 {
-unsigned long int newr;
-unsigned char lobit;
-unsigned char b31, b29, b25, b24;
-b31 = (reg & (1L << 31)) >> 31;
-b29 = (reg & (1L << 29)) >> 29;
-b25 = (reg & (1L << 25)) >> 25;
-b24 = (reg & (1L << 24)) >> 24;
-lobit = b31 ^ b29 ^ b25 ^ b24;
-newr = (reg << 1) | lobit;
-reg = newr;
-digitalWrite (speakerPin, reg & 1);
-delayMicroseconds (50); //changing this val changes frequency
+    unsigned long int reg;
+    unsigned long int newr;
+    unsigned char lobit;
+    unsigned char b31, b29, b25, b24;
+    b31 = (reg & (1L << 31)) >> 31;
+    b29 = (reg & (1L << 29)) >> 29;
+    b25 = (reg & (1L << 25)) >> 25;
+    b24 = (reg & (1L << 24)) >> 24;
+    lobit = b31 ^ b29 ^ b25 ^ b24;
+    newr = (reg << 1) | lobit;
+    reg = newr;
+    digitalWrite (speakerPin, reg & 1);
+    delayMicroseconds (50); //changing this val changes frequency
 }
 
 
 unsigned int generateNoise_b()
 {
-// see "linear feedback shift register, Galois LFSR"
-static unsigned long int lfsr = LFSR_INIT; //start with 32 bits, non-zero
-if(lfsr & 1)
-{
-    lfsr = (lfsr>>1) ^ LFSR_MASK;
-    return(1);
-}
-else
-{
-    lfsr >>= 1;
-    return(0);
+    // see "linear feedback shift register, Galois LFSR"
+    static unsigned long int lfsr = LFSR_INIT; //start with 32 bits, non-zero
+    if(lfsr & 1)
+    {
+        lfsr = (lfsr>>1) ^ LFSR_MASK;
+        return(1);
+    }
+    else
+    {
+        lfsr >>= 1;
+        return(0);
+    }
 }
 
 
@@ -57,7 +59,7 @@ void loop()
     if ((micros() - lastClick) > 50)
     {
         lastClick = micros();
-        digitalWrite (speakerPin, generateNoise());
+        digitalWrite (speakerPin, generateNoise_b());
     }
 }
 
