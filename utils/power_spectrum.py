9f4083e3e37b39bb849c8223258bafda82d68c66
Brendan Hayward <brendanhayward85@gmail.com>
2017-06-17 14:20:34 +1000

    refactor power_spectrum

--- utils/power_spectrum.py
+++ utils/power_spectrum.py
@@ -2,13 +2,21 @@ from matplotlib import pyplot as plt
 import numpy as np
 import soundfile as sf
 data = np.random.random(1000000)
-with sf.SoundFile('soundtest.wav', 'x+', 64000, 2) as noisefile:
-    noisefile.write(np.array([data, data]).transpose())
+
+def make_file(data):
+    with sf.SoundFile('soundtest.wav', 'x+', 64000, 2) as noisefile:
+        noisefile.write(np.array([data, data]).transpose())
 
 def plot_power_spectrum(data):
     pwr_spec = np.abs(np.fft.fft(data))**2
-    time_step = 1/30
+    time_step = 1
     freqs = np.fft.fftfreq(data.size, time_step)
     index = np.argsort(freqs)
     plt.plot(freqs[index], pwr_spec[index])
-    #plt.show()  # may want to do this in calling scope
+    plt.show()  # may want to do this in calling scope
+
+def main():
+    plot_power_spectrum(data)
+
+if __name__ == '__main__':
+    main()
